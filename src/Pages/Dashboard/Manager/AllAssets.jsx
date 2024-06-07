7bf2f2da2d5bf31bf51ea08822d3b4eb5d1ee5b2
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-06-07 17:05:48 +0600

    filter,search and sort button added in allAssets Page

--- src/Pages/Dashboard/Manager/AllAssets.jsx
+++ src/Pages/Dashboard/Manager/AllAssets.jsx
@@ -8,7 +8,39 @@ const AllAssets = () => {
     return (
         <div className='w-4/5 mx-auto text-center my-16'>
             <Title heading={'ASSET LIST'} subHeading={'All the Returnable and Non-Returnable Assets are listed here.'} ></Title>
-            <h2 className="text-2xl mb-6 text-purple-500">Total Assets: {assets.length} </h2>
+            <h2 className="text-2xl mb-4 text-purple-500">Total Assets: {assets.length} </h2>
+            {/* SECTION------*/}
+            <div className='mb-8 flex flex-col md:flex-row justify-center w-5/6 md:w-1/2 mx-auto'>
+                {/* Filter ANd Search------- */}
+                <div className="join">
+                    <div className='w-1/3 md:full'>
+                        <div>
+                            <input className="input input-bordered join-item" placeholder="Search"/>
+                        </div>
+                    </div>
+                    <select className="select select-bordered join-item w-1/3 md:full">
+                        <option disabled selected>Filter</option>
+                        <option>Available</option>
+                        <option>Out-Of-Stock</option>
+                    </select>
+                    <div className="indicator w-1/3 md:full">
+                        <button className="btn join-item btn-base">Search</button>
+                    </div>
+                </div>
+                {/* ----SORT---SECTION----*/}
+                <div className='mt-4 md:mt-0'>
+                    <div className="join">
+                        <select className="select select-bordered join-item">
+                            <option disabled selected>Sort</option>
+                            <option>Quantity</option>
+                        </select>
+                        <div className="indicator">
+                            <button className="btn join-item btn-base">Sort</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* Grid of Assets */}
             <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
                 {assets.map((asset)=> 
                     <Asset 
--- src/Pages/Dashboard/Manager/Asset.jsx
+++ src/Pages/Dashboard/Manager/Asset.jsx
@@ -7,15 +7,19 @@ const Asset = ({asset}) => {
             <figure><img className="w-[400px] h-[240px]" src={image} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">
-                {name}
-                <div className="badge badge-secondary">{stock}</div>
+                    {name}
+                    <div className="badge badge-primary">{stock}</div>
                 </h2>
-                <p>Type: {type}</p>
-                <p>Quantity: {quantity}</p>
-                <p>Date Added: {date_added}</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-outline text-purple-700 font-bold btn-sm">Update</button>
-                    <button className="btn btn-outline text-red-500 btn-sm">Delete</button>
+               <div className="grid grid-cols-2 gap-4 text-left">
+                    <p className="text-gray-600">Type: <span className="font-semibold text-purple-500">{type}</span> </p>
+                    <p className="text-gray-600">Quantity: <span className="font-semibold text-purple-500">{quantity}</span> </p>
+               </div>
+              <div className="mb-2">
+                    <p className="text-gray-600 text-left">Date Added: <span className="font-semibold text-purple-500">{date_added}</span> </p>
+              </div>
+                <div className="flex gap-2 justify-evenly">
+                    <button className="btn btn-outline text-purple-700 font-bold btn-sm w-1/2">Update</button>
+                    <button className="btn btn-outline text-red-500 btn-sm w-1/2">Delete</button>
                 </div>
             </div>
         </div>
