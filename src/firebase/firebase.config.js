1e45600c4d087560cee7d30e6a33e6ba952a9641
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-06-09 01:57:03 +0600

    firebase configuration set up complete

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,8 +5,8 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_authDomain,
-  authDomain: import.meta.env.VITE_apiKey,
+  apiKey: import.meta.env.VITE_VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
   projectId: import.meta.env.VITE_projectId,
   storageBucket: import.meta.env.VITE_storageBucket,
   messagingSenderId: import.meta.env.VITE_messagingSenderId,
